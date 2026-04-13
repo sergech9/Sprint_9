3de61d2149899caebbef0f89aebc66317624daec
Sergei Chernov <sergeu.amik@gmail.com>
2026-04-13 08:21:59 +0300

    fix7

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,7 +8,7 @@ from selenium.common.exceptions import TimeoutException
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(driver, 20)
 
     def open(self, url):
         self.driver.get(url)
